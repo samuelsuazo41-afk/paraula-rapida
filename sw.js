910115fae1c566f278191d1bd8f6effa8d585436
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-28 15:48:28 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,56 @@
-// sw.js - Paraula Ràpida v2.11
-const CACHE_NAME = 'paraula-rapida-v30';
+// sw.js v3.3 - Paraula Ràpida
+
+const CACHE_NAME = 'paraula-rapida-v33';
+
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './main.js',
-  './lectura-generator.js',
+  './lectura-generator.js',  // Generador de lectura
+  './lectura-content.js',    // Contingut de lectura - NOU
   './emoji-data.js',
   './frases-data.js',
   './tips-data.js',
   './botiga-data.js',
+  './icon-96.png',
   './icon-192.png',
   './icon-512.png',
   './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+// === INSTALL: cacheja tot ===
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('SW: Cachejant v33');
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', e => {
+// === ACTIVATE: esborra caché antic ===
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// === FETCH: cache first ===
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => {
+      return resp || fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
   );
 });
