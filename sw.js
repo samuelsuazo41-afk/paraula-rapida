05f8196116950ea3091cbca99f76cf34c036d883
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-28 18:16:41 +0200

    Actualizar sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'paraula-rapida-v45';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './main.js',
+  './emoji-data.js',
+  './botiga-data.js',
+  './tips-data.js',
+  './frases-data.js',
+  './lectura-content.js',
+  './lectura-generator.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
